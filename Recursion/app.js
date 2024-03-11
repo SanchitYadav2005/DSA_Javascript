b5ceb84ce0bacd209730fde0e9b970d7778d6894
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-11 07:13:42 +0530

    worked on recursion

--- Recursion/app.js
+++ Recursion/app.js
@@ -23,3 +23,60 @@ function factorial(fact) {
 }
 let factData = 5;
 console.log(factorial(factData));
+
+//inderict recursion- Where several functions depends upon one another. Just like they call each other.
+
+let money = 100;
+let totalApple = 0;
+
+function buyApple(priceForOneApple) {
+  if (priceForOneApple > 0) {
+    console.log(`I have ${priceForOneApple} rs ${totalApple}`);
+    buyMore(priceForOneApple);
+  } else {
+    console.log(`I don't have more money to buy, ${totalApple}`);
+  }
+}
+
+function buyMore(priceForOneApple) {
+  totalApple++;
+  buyApple(priceForOneApple - 5);
+}
+
+buyApple(money);
+
+//head recursion- If a recursive function is calling itself and that recursive call is the first statement in the function in the function then its known as Head recursion.
+
+function random(x) {
+  console.log(x);
+  if (x > 0) {
+    random(x - 1);
+  }
+}
+random(5);
+
+// tail recursion- If a recursive function calling itself and that recursive call is the last statement in the function then its known as tail recursion.
+function random2(x) {
+  if (x > 0) {
+    random2(x - 1);
+  }
+  console.log(x);
+}
+random2(5);
+
+// reversing an array using recursion
+
+let data = [43, 45, 652, 432, 64, 12, 54];
+let temp;
+
+function reverseData(data, start, end) {
+  console.log(data);
+  if (start <= end) {
+    temp = data[start];
+    data[start] = data[end];
+    data[end] = temp;
+    reverseData(data, start + 1, end - 1);
+  }
+}
+
+reverseData(data, 0, data.length - 1);
